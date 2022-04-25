d61641598738c881ffbfb340bf99694eac4c4ea6
Dasun Sucharith <sucharith.dasun@gmail.com>
2022-04-25 23:13:23 +0530

    new

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -5,6 +5,38 @@ import AVTR2 from '../../assets/avatar1.png'
 import AVTR3 from '../../assets/avatar1.png'
 import AVTR4 from '../../assets/avatar1.png'
 
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination'; 
+
+
+const data = [
+  {
+    avatar: AVTR1,
+    name: 'Tina Snow',
+    review: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi magni inventore explicabo corrupti totam! Sed porro nisi aliquid, nihil optio, soluta doloremque quidem libero iusto dolorum alias ipsam ea ullam!',
+  },
+  {
+    avatar: AVTR2,
+    name: 'John Knight',
+    review: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi magni inventore explicabo corrupti totam! Sed porro nisi aliquid, nihil optio, soluta doloremque quidem libero iusto dolorum alias ipsam ea ullam!',
+  },
+  {
+    avatar: AVTR3,
+    name: 'Rebecca Rubasinghe',
+    review: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi magni inventore explicabo corrupti totam! Sed porro nisi aliquid, nihil optio, soluta doloremque quidem libero iusto dolorum alias ipsam ea ullam!',
+  },
+  {
+    avatar: AVTR4,
+    name: 'Pasindu Akalanka',
+    review: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi magni inventore explicabo corrupti totam! Sed porro nisi aliquid, nihil optio, soluta doloremque quidem libero iusto dolorum alias ipsam ea ullam!',
+  }
+]
 
 function Testimonials() {
   return (
@@ -13,13 +45,19 @@ function Testimonials() {
       <h2>Testimonials</h2>
 
       <div className="container testimonials__container">
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={AVTR1} alt="Avatar One" />
-          </div>
-          <h5 className='client__name'>Ernest Achiever</h5>
-          <small className="client__review">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, natus suscipit laborum odit, distinctio temporibus aperiam, nostrum tempore quisquam dolorum voluptatibus iusto rem numquam enim quia maiores quaerat! Ullam, temporibus.</small>
-        </article>
+        {
+          data.map(({avatar, name, review}, index) => {
+            return (
+              <article key={index} className="testimonial">
+                <div className="client__avatar">
+                  <img src={avatar} alt="Avatar One" />
+                </div>
+                <h5 className='client__name'>{name}</h5>
+                <small className="client__review">{review}</small>
+              </article>
+            )
+          })
+        }
       </div>
     </section>
   )
