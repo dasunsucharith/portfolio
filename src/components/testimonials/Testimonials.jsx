b89d599533ee7ea9f15b78d44b75e3e452d5c67e
Dasun Sucharith <sucharith.dasun@gmail.com>
2022-04-26 05:41:18 +0530

    new

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -6,13 +6,14 @@ import AVTR3 from '../../assets/avatar1.png'
 import AVTR4 from '../../assets/avatar1.png'
 
 // import Swiper core and required modules
-import { Pagination } from 'swiper';
+import { Pagination, Autoplay } from 'swiper';
 
 import { Swiper, SwiperSlide } from 'swiper/react'
 
 // Import Swiper styles
 import 'swiper/css';
-import 'swiper/css/pagination'; 
+import 'swiper/css/pagination';
+import 'swiper/css/autoplay';
 
 
 const data = [
@@ -44,21 +45,29 @@ function Testimonials() {
       <h5>Review From Clients</h5>
       <h2>Testimonials</h2>
 
-      <div className="container testimonials__container">
+      <Swiper 
+        className="container testimonials__container"
+        // install Swiper modules
+        modules={[ Pagination, Autoplay ]}
+        spaceBetween={30}
+        slidesPerView={1}
+        autoplay={{delay:5000}}
+        pagination={{ clickable: true }}
+      >
         {
           data.map(({avatar, name, review}, index) => {
             return (
-              <article key={index} className="testimonial">
+              <SwiperSlide key={index} className="testimonial">
                 <div className="client__avatar">
                   <img src={avatar} alt="Avatar One" />
                 </div>
                 <h5 className='client__name'>{name}</h5>
                 <small className="client__review">{review}</small>
-              </article>
+              </SwiperSlide>
             )
           })
         }
-      </div>
+      </Swiper>
     </section>
   )
 }
