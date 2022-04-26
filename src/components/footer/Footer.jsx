269712bd80311073364af05de26e7ab3a9eb8470
Dasun Sucharith <sucharith.dasun@gmail.com>
2022-04-26 18:32:18 +0530

    commit

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './Footer.css'
+import {FaFacebookF} from 'react-icons/fa'
+import {FiInstagram} from 'react-icons/fi'
+import {IoLogoTwitter} from 'react-icons/fi'
 
 function Footer() {
   return (
@@ -8,13 +11,19 @@ function Footer() {
 
       <ul className="permalinks">
         <li><a href="#home">Home</a></li>
-        <li><a href="#about"></a></li>
-        <li><a href="#experience"></a></li>
-        <li><a href="#services"></a></li>
-        <li><a href="#portfolio"></a></li>
-        <li><a href="#testimonials"></a></li>
-        <li><a href="#contact"></a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
       </ul>
+
+      <div className="footer__socials">
+        <a href="https://facebook.com"></a>
+        <a href="https://instagram.com"></a>
+        <a href="https://twitter.com"></a>
+      </div>
     </footer>
   )
 }
