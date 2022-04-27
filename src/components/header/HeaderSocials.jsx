cda81b1a0a0d141fbccd0ddaf6f1db9664300d39
Dasun Sucharith <sucharith.dasun@gmail.com>
2022-04-27 08:04:01 +0530

    social

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -7,8 +7,8 @@ import {FiDribbble} from 'react-icons/fi'
 const HeaderSocials = () => {
     return (
         <div className='header__socials'>
-            <a href="http://linkedin.com" target="_blank" rel="noopener noreferrer"><BsLinkedin/></a>
-            <a href="http://github.com" target="_blank" rel="noopener noreferrer"><FaGithub/></a>
+            <a href="https://www.linkedin.com/in/dasun-sucharith-796777110/" target="_blank" rel="noopener noreferrer"><BsLinkedin/></a>
+            <a href="https://github.com/dasunsucharith" target="_blank" rel="noopener noreferrer"><FaGithub/></a>
             <a href="http://dribbble.com" target="_blank" rel="noopener noreferrer"><FiDribbble/></a>
         </div>
     )
